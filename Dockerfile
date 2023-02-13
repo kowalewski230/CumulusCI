e4db49d9c8c055ef913eca4eab25b5b175e01a84
Krzysztof Kowalewski <krzysztof.kowalewski@ttms.pl>
2023-02-13 15:53:55 +0000

    added

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,11 @@ WORKDIR /usr/src/app
 # RUN python -m pip install --upgrade pip pip-tools setuptools
 # RUN pip-sync requirements/*.txt
 # RUN python -m pip install -e .
-RUN wget https://developer.salesforce.com/media/salesforce-cli/sfdx/channels/stable/sfdx-linux-x64.tar.xz
-RUN mkdir ~/sfdx
-RUN tar xJf sfdx-linux-x64.tar.xz -C ~/sfdx --strip-components 1
-RUN export PATH=~/sfdx/bin:$PATH
-RUN PATH=~/sfdx/bin:$PATH
+# RUN wget https://developer.salesforce.com/media/salesforce-cli/sfdx/channels/stable/sfdx-linux-x64.tar.xz
+# RUN mkdir ~/sfdx
+# RUN tar xJf sfdx-linux-x64.tar.xz -C ~/sfdx --strip-components 1
+# RUN export PATH=~/sfdx/bin:$PATH
+# RUN PATH=~/sfdx/bin:$PATH
 ADD . .
 RUN make dev-install
 
--- orgdatainit.sh
+++ orgdatainit.sh
@@ -1,4 +1,6 @@
 #!/bin/bash  
+cci org connect myTestSandbox --sandbox  
+
 cci task run snowfakery --recipe OrgDataInit/lead.yml  --run-until-recipe-repeated 1 --org myTestSandbox --debug
 # cci task run snowfakery --recipe OrgDataInit/account.yml 
 cci task run snowfakery --recipe OrgDataInit/account.yml  --run-until-recipe-repeated 1  --org myTestSandbox --debug 
@@ -7,7 +9,7 @@ cci task run snowfakery --recipe OrgDataInit/account.yml  --run-until-recipe-rep
 cci task run capture_sample_data --org myTestSandbox
 
 # Delete All data 
-cci task run delete_data -o objects Opportunity,Contact,Account,Lead,Product2,ContentDocument,Task --org myTestSandbox
+cci task run delete_data -o objects Opportunity,Account,Entitlement,Case,Lead,Product2,ContentDocument,Task,Event --org myTestSandbox --ignore_row_errors true
 # Load All data
 cci task run load_sample_data --org myTestSandbox
 
